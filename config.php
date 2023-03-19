cf648e1a169c34367f8f8e317d2ffc66250bd273
Raj girase <rajgirase2012@gmail.com>
2023-03-19 11:44:47 +0530

    first commit

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+define('CURRENCY', '$');
+define('WEB_URL', 'http://127.0.0.1/apartment/');
+define('ROOT_PATH', 'I:/PythonXAMPP/htdocs/Apartment/');
+
+
+define('DB_HOSTNAME', '127.0.0.1');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'ams_db');
+$link = mysqli_connect('localhost', 'root', '', 'ams_db');
